6608262567c37d4f197f8db274cbecf5277a2e38
danieljimenez-palacios <daniel815jimenez@gmail.com>
2024-10-20 11:17:22 -0400

    Initial commit

---
+++ src/Message.java
@@ -0,0 +1,15 @@
+
+public class GetMessage() {
+    public static void getHappy(String tdaysFeel, String happyM) {
+    }
+    public static void getSad(String tdaysFeel, String sadM) {
+
+    }
+    public static void getFear(String tdaysFeel, String fearM) {
+
+    }
+    public static void getAngry(String tdaysFeel, String angryM) {
+
+    }
+
+}
