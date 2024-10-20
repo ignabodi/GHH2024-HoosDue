488e04181d0923135d63fab69eaf417af35da6ff
danieljimenez-palacios <daniel815jimenez@gmail.com>
2024-10-20 11:17:22 -0400

    i made a lil template for you

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    //FATIMAH THIS IS WHERE YOU START YOUR METICULOUS JOURNEY OF BANGING YOUR HEAD TRYING TO FIGURE OUT HOW TO DO SOMETHING
+        //  *REPLACE THIS WHOLE LINE WITH A GREETING TO THE USER AND WELCOMING THEM AND DECLARE A SCANNER*
+
+
+        // *then ask them to choose(TYPE) from a list of emotions angry, happy, sad, anxious, scared, good, bad*
+
+
+        // *then ask them if they want to be pushed or if they want to be comforted*
+
+
+        //REMEMBER TO TAKE THEIR ANSWER TO EACH OF THESE QUESTIONS AND STORE THEM BY DECLARING A STRING = TO THEIR ANSWER*
+    }
+}
---
+++ src/Message.java
@@ -0,0 +1,20 @@
+
+public class Message {
+    public static void getMessage(String tdaysFeel) {
+
+        if (tdaysFeel.equals("happy")) {
+            System.out.p;
+        }
+    }
+    public static void getSad(String tdaysFeel, String sadM) {
+
+    }
+    public static void getFear(String tdaysFeel, String fearM) {
+
+    }
+    public static void getAngry(String tdaysFeel, String angryM) {
+
+    }
+
+
+}
