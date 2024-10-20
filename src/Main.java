47a896601f06a147cfb4a02e3d3396adb32c68a1
danieljimenez-palacios <daniel815jimenez@gmail.com>
2024-10-20 13:12:06 -0400

    all done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
+import java.util.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-    //FATIMAH THIS IS WHERE YOU START YOUR METICULOUS JOURNEY OF BANGING YOUR HEAD TRYING TO FIGURE OUT HOW TO DO SOMETHING
-        //  *REPLACE THIS WHOLE LINE WITH A GREETING TO THE USER AND WELCOMING THEM AND DECLARE A SCANNER*
+    public static void main(String[] args) {//  *REPLACE THIS WHOLE LINE WITH A GREETING TO THE USER AND WELCOMING THEM AND DECLARE A SCANNER
+        //hi, life is kinda crazy sometimes and we could all use some kind words every once in a while
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hi there, life is kinda crazy sometimes and we could all use kind words every once in a while. What's your name?");
+        String name = sc.nextLine();
+
+        System.out.println("Nice to meet you " + name + ", I know there might be many ways to describe how you're feeling right now but try condensing it to one of these: happy, angry, sad, anxious, scared, excited, good, or bad?");
+        String feeling = sc.nextLine();
+
+        System.out.println("And lastly, do you want to be comforted or pushed?");
+        String messageType = sc.nextLine();
+
+        Message userInfo = new Message(name, feeling, messageType);
+        userInfo.getKindWords();
+
+
+        /*private static String getMessageType(name) {
+            System.out.println("Hi there, life is kinda crazy sometimes and we could all use some kinda words every once in a while. What's your name?");
+            String name = sc.nextLine();
+        }
+        private static String getUsersFeeling() {
+            System.out.println("I know there might be many ways to describe how you're feeling right now but type the word which best describes the feeling: happy, angry, sad, anxious, scared, excited, good, or bad");
+            String userFeeling = sc.nextLine();
+
+        }
+
+
 
 
         // *then ask them to choose(TYPE) from a list of emotions angry, happy, sad, anxious, scared, good, bad*
@@ -13,6 +39,6 @@ public class Main {
 
 
 
-        //REMEMBER TO TAKE THEIR ANSWER TO EACH OF THESE QUESTIONS AND STORE THEM BY DECLARING A STRING = TO THEIR ANSWER*
+        //REMEMBER TO TAKE THEIR ANSWER TO EACH OF THESE QUESTIONS AND STORE THEM BY DECLARING A STRING = TO THEIR ANSWER*/
     }
 }
--- src/Message.java
+++ src/Message.java
@@ -1,19 +1,88 @@
 
 public class Message {
-    public static void getMessage(String tdaysFeel) {
+    private final String name;
+    private final String feeling;
+    private final String messageType;
 
-        if (tdaysFeel.equals("happy")) {
-            System.out.p;
+    public Message(String name, String feeling, String messageType) {
+        this.name = name;
+        this.feeling = feeling;
+        this.messageType = messageType;
+
+    }
+
+    public String getKindWords() {
+        String message;
+        switch (feeling.toLowerCase()) {
+            case "happy":
+                if (messageType.equals("comforted")) {
+                    message = ", Keep your head up and remember all the greatness that you are capable of achieving! Your a women in STEM and that makes you unique, you are untouchable! Keep up the good work :)";
+                }
+                else {
+                    message = ", Your energy is contagious, I can feel it through the screen! Uplift the people around you and remember that you are capable of more than you know in every aspect of life!";
+                }
+                break;
+            case "angry":
+                if (messageType.equals("comforted")) {
+                    message = ", I am sorry your feeling put down, but remember that after every storm comes a rainbow :) this feeling will pass and its within your control! Go take a break, do something that fills your heart, we are human after all!";
+                }
+                else {
+                    message = ", I am sorry your feeling angry, sometimes it helps to channel that feeling into other parts of life. Go run a mile, hug a tree, or sing your favorite song with all your chest! This feeling will pass, and its up to you to decide how.";
+                }
+                break;
+            case "sad":
+                if (messageType.equals("comforted")) {
+                    message = ", I am sorry your feeling sad. Sometimes people just need a good cry. Let out your sadness in whichever way you can, talk to your family, write down what made you sad and how you can fix it, or get cozy and watch a movie. You are in control of your fate, you've got this girl!";
+                }
+                else {
+                    message = ", I am sorry your feeling sad. Remember that this feeling will pass and you have full control over your fate. This emotion does not define you!";
+                }
+                break;
+            case "anxious":
+                if (messageType.equals("comforted")) {
+                    message = ", Breath and remember that you can get through this! ";
+                }
+                else {
+                    message = ", this isn't the first time you've been anxious and it definitely wont be the last. Get up and control the things you can, and let go of the things you cant.";
+                }
+                break;
+            case "scared":
+                if (messageType.equals("comforted")) {
+                    message = ", I'm sorry you're scared, remember that with every darkness, theres is a light at the end of the tunnel.";
+                }
+                else {
+                    message = ", we didn't come this far just to let a little fear bring us down";
+                }
+                break;
+            case "excited":
+                if (messageType.equals("comforted")) {
+                    message = ", LETSS GOOOO KEEP THE BALL ROLLING!!!";
+                }
+                else {
+                    message = ", OH MY GOD WHATS NEXT!!!";
+                }
+                break;
+            case "good":
+                if (messageType.equals("comforted")) {
+                    message = ", keep doing the things you've been doing because they are what make you, you.";
+                }
+                else {
+                    message = ", thats awesome! Dont stop working at getting better!";
+                }
+                break;
+            case "bad":
+                if (messageType.equals("comforted")) {
+                    message = ", I'm sorry to hear that boo, just remember everything happens for a reason and like Katy Perry said --after a hurrican comes a rainbow :)";
+                }
+                else {
+                    message = ", keep pushing and be the person who thought they couldn't but did, not the person who thought they could've but didnt.";
+                }
+                break;
+            default:
+                message = "try to describe your feeling in the way I suggested";
         }
-    }
-    public static void getSad(String tdaysFeel, String sadM) {
 
-    }
-    public static void getFear(String tdaysFeel, String fearM) {
-
-    }
-    public static void getAngry(String tdaysFeel, String angryM) {
-
+        return name + message;
     }
 
 
